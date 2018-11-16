996df24de3a481180f88e15bca5aac28828bbb5e
Isaiah Noah Santos <isaiahnoah.santos@uoit.net>
2018-11-16 14:08:34 -0500

    Overworld movement working

--- Pokemon ASCII/Pokemon ASCII/DrawGym.cpp
+++ Pokemon ASCII/Pokemon ASCII/DrawGym.cpp
@@ -4,7 +4,9 @@
 void DrawGym::display()
 {
 	drawGym(x,y);
-	movement();
+	for(;;)
+		movement();
+	
 }
 void DrawGym::drawGym(int charX,int charB)
 {
@@ -28,44 +30,45 @@ void DrawGym::movement()
 {
 	char input = 0;
 	input = _getch();//used to read a character from screen without using enter
-	for (;;)
+	switch (input)
 	{
-		switch (input)
+	case 'w':
+		system("CLS");
+		//std::cout << "W was pressed \n";
+		//break;
+		if (y - 1 >= 0)
 		{
-		case 'w':
-			system("CLS");
-			std::cout << "W was pressed \n";
-			break;
-			//if (y - 1 >= 0)
-			//{
-			//	y--;
-			//	drawGym(x, y);
-			//}
-			//break;
-		case 'a':
-			system("CLS");
-			std::cout << "A was pressed \n";
-			break;
-			//if (x - 1 >= 0)
-			//{
-			//	x--;
-			//	drawGym(x, y);
-			//}
-			//break;
-		case 's':
-			system("CLS");
-			std::cout << "S was pressed \n";
-			break;
-			//drawGym(x, y);
-			//break;
-		case 'd':
-			system("CLS");
-			std::cout << "D was pressed \n";
-			break;
-			//drawGym(x, y);
-			//break;
+			y--;
+			drawGym(x, y);
 		}
-		
+		break;
+	case 'a':
+		system("CLS");
+		if (x - 1 >= 0)
+		{
+			x--;
+			drawGym(x, y);
+		}
+		break;
+	case 's':
+		system("CLS");
+		if (y + 1 <=24)
+		{
+			y++;
+			drawGym(x, y);
+		}
+		break;
+		//drawGym(x, y);
+		//break;
+	case 'd':
+		system("CLS");
+		if (y + 1 <=24)
+		{
+			x++;
+			drawGym(x, y);
+		}
+		break;
 	}
-	
+
 }
+	
